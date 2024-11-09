d1fcb19e27c96183ebce9aaf4780577640874bef
IveryChen <ivy_chen1@brown.edu>
2024-11-09 15:34:25 -0800

    use React Async in SpotifyAuth

--- frontend/src/components/SpotifyAuth/index.jsx
+++ frontend/src/components/SpotifyAuth/index.jsx
@@ -1,9 +1,43 @@
 import { Component } from "react";
+import Async from "react-async";
 
-class SpotifyAuth extends Component {
+import { initializeSpotify } from "../../library/spotify";
+
+export default class SpotifyAuth extends Component {
+  componentDidMount() {
+    this.handleInitialAuth();
+  }
+
+  handleInitialAuth = async () => {
+    const spotify = initializeSpotify();
+    if (!spotify.getAccessToken()) {
+      await this.handleLogin();
+    }
+  };
+
+  handleLogin = async () => {
+    try {
+      // Fetch login URL from the backend
+      const response = await fetch("http://localhost:3000/login");
+      const data = await response.json();
+      console.log("data.url", data);
+      // Redirect to Spotify login
+      window.location.href = data.url;
+    } catch (error) {
+      console.error("Failed to get login URL:", error);
+    }
+  };
+
   render() {
-    return <div>my Auth UI</div>;
+    return (
+      <Async promiseFn={this.handleInitialAuth}>
+        {({ data, error, isPending }) => {
+          if (isPending) return <p>Loading...</p>;
+          if (error) return <p>Error: {error.message}</p>;
+          if (data) return <p>Spotify authenticated!</p>;
+          return null;
+        }}
+      </Async>
+    );
   }
 }
-
-export default SpotifyAuth;
