03f95ad5f778a50879015b8e642e030e20ed2c91
IveryChen <ivy_chen1@brown.edu>
2024-11-09 13:44:04 -0800

    clean up SpotifyAuth

--- frontend/src/components/SpotifyAuth/index.jsx
+++ frontend/src/components/SpotifyAuth/index.jsx
@@ -1,29 +1,6 @@
 import { Component } from "react";
-import { initializeSpotify } from "../lib/spotify";
 
 class SpotifyAuth extends Component {
-  componentDidMount() {
-    this.handleInitialAuth();
-  }
-
-  handleInitialAuth = async () => {
-    const spotify = initializeSpotify();
-    if (!spotify.getAccessToken()) {
-      await this.handleLogin();
-    }
-  };
-
-  handleLogin = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/login");
-      const data = await response.json();
-      // Redirect to Spotify login
-      window.location.href = data.url;
-    } catch (error) {
-      console.error("Failed to get login URL:", error);
-    }
-  };
-
   render() {
     return <div>my Auth UI</div>;
   }
