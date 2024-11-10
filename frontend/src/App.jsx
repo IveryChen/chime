c2998e6ce5a552797d1197a9368f49c73890ae11
IveryChen <ivy_chen1@brown.edu>
2024-11-09 16:09:56 -0800

    add / route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import SpotifyAuth from "./components/SpotifyAuth";
+
 import Home from "./routes/Home";
 import Lobby from "./routes/Lobby";
 
@@ -11,6 +13,7 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<SpotifyAuth />} />
           <Route path="/home" element={<Home />} />
           <Route path="/lobby" element={<Lobby />} />
         </Routes>
