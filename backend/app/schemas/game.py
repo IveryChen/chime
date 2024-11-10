874683b131c521a4330ce10b0a395a25e8c47d68
IveryChen <ivy_chen1@brown.edu>
2024-11-10 15:32:05 -0800

    modify schemas to for createRoomRequest so it takes camelCase

--- backend/app/schemas/game.py
+++ backend/app/schemas/game.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 class Player(BaseModel):
@@ -18,5 +18,8 @@ class JoinRoomRequest(BaseModel):
     spotify_token: str
 
 class CreateRoomRequest(BaseModel):
-    host_name: str
-    spotify_token: str
+    host_name: str = Field(alias="hostName")
+    spotify_token: str = Field(alias="spotifyToken")
+
+    class Config:
+        allow_population_by_field_name = True
