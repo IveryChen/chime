2e2302a72cddc25513efd95f7a31ca1696178a04
IveryChen <ivy_chen1@brown.edu>
2024-11-10 14:19:44 -0800

    add check to check localStorage first before checking URL params

--- frontend/src/library/spotify.js
+++ frontend/src/library/spotify.js
@@ -3,10 +3,19 @@ import SpotifyWebApi from "spotify-web-api-js";
 const spotifyApi = new SpotifyWebApi();
 
 export const initializeSpotify = () => {
+  // First check localStorage
+  const storedToken = localStorage.getItem("spotify_access_token");
+  if (storedToken) {
+    spotifyApi.setAccessToken(storedToken);
+    return spotifyApi;
+  }
+
+  // Then check URL params
   const params = new URLSearchParams(window.location.search);
   const accessToken = params.get("access_token");
 
   if (accessToken) {
+    localStorage.setItem("spotify_access_token", accessToken);
     spotifyApi.setAccessToken(accessToken);
   }
 
