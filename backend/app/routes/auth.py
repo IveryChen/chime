f6704f0add5e9e905624f51acd60ba114de04834
IveryChen <ivy_chen1@brown.edu>
2024-11-10 23:49:05 -0800

    remove line in auth.py

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -11,7 +11,7 @@ async def login():
         auth_manager = SpotifyOAuth(
             client_id=settings.SPOTIFY_CLIENT_ID,
             client_secret=settings.SPOTIFY_CLIENT_SECRET,
-            redirect_uri=settings.SPOTIFY_REDIRECT_URI,  # This should be http://localhost:8000/api/auth/callback
+            redirect_uri=settings.SPOTIFY_REDIRECT_URI,
             scope="user-read-private user-read-email user-library-read playlist-read-private user-top-read"
         )
         print("Redirect URI:", settings.SPOTIFY_REDIRECT_URI)  # Debug print
