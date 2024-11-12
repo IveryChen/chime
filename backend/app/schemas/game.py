b2493bd6e0692bb563cc20b781bd463cfa4046d3
IveryChen <ivy_chen1@brown.edu>
2024-11-12 00:25:07 -0800

    update allow_population_by_field_name

--- backend/app/schemas/game.py
+++ backend/app/schemas/game.py
@@ -22,4 +22,4 @@ class CreateRoomRequest(BaseModel):
     spotify_token: str = Field(alias="spotifyToken")
 
     class Config:
-        allow_population_by_field_name = True
+        populate_by_name = True
