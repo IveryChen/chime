0efae7b0420f768d681eb420e634d207da2dc408
IveryChen <ivy_chen1@brown.edu>
2024-11-13 23:38:33 -0800

    add specific header types in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.allowed_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
     expose_headers=["*"]
 )
 
