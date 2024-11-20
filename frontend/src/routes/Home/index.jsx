8d84f01e07afa3f545064397990124bcac328012
IveryChen <ivy_chen1@brown.edu>
2024-11-20 01:06:18 -0800

    add button and styling to Home page

--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { FaSpotify } from "react-icons/fa6";
 
 import Box from "../../components/Box";
 import Text from "../../components/Text";
@@ -8,30 +9,53 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Box display="grid" gap="32px" padding="32px">
-        <Text
-          fontWeight="bold"
-          fontSize="120px"
-          lineHeight={1}
-          letterSpacing="-2px"
-          width="60%"
-        >
-          YOUR GO-TO SPOTIFY MUSIC GAME.
-        </Text>
-        <Text
-          bg="#4CABFF"
-          borderColor="black"
-          borderRadius="20px"
-          borderStyle="solid"
-          borderWidth={2}
-          fontFamily="Bebas Neue"
-          fontSize="20px"
-          justifySelf="start"
-          onClick={this.onClick}
-          p="8px"
-        >
-          I’M READY TO CUE IN
-        </Text>
+      <Box height="100%">
+        <Box display="grid" gridTemplateColumns="auto 1fr" height="100%">
+          <Box display="grid" gap="24px">
+            <Text
+              fontWeight="bold"
+              fontSize="120px"
+              lineHeight={1}
+              letterSpacing="-2px"
+              width="60%"
+            >
+              YOUR GO-TO SPOTIFY MUSIC GAME.
+            </Text>
+            <Text
+              alignSelf="start"
+              bg="#4CABFF"
+              borderColor="black"
+              borderRadius="20px"
+              borderStyle="solid"
+              borderWidth={2}
+              cursor="pointer"
+              display="flex"
+              fontFamily="Bebas Neue"
+              fontSize="20px"
+              gap="8px"
+              justifySelf="start"
+              onClick={this.onClick}
+              p="8px"
+              userSelect="none"
+            >
+              I’M READY TO CUE IN
+              <Box alignSelf="center" as={FaSpotify} size={24} />
+            </Text>
+          </Box>
+          <Box
+            borderColor="black"
+            borderRadius="20px"
+            borderStyle="solid"
+            borderWidth={2}
+            display="flex"
+            justifyContent="space-between"
+            bg="#F7FFF9"
+            width="64px"
+            height="100%"
+          >
+            <Text>CASSETTE</Text>
+          </Box>
+        </Box>
       </Box>
     );
   }
