d3ebf7a0dee78d7181cb6999fc479fb4234f9c89
IveryChen <ivy_chen1@brown.edu>
2024-11-19 22:34:09 -0800

    style Home page

--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -1,15 +1,38 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+
+import Box from "../../components/Box";
+import Text from "../../components/Text";
 
 export default class Home extends Component {
+  onClick = () => (window.location.href = "/spotify-auth");
+
   render() {
     return (
-      <div>
-        <h1>Welcome to Chime</h1>
-        <Link to="/spotify-auth">
-          <button>Login with Spotify</button>
-        </Link>
-      </div>
+      <Box display="grid" gap="32px" padding="32px">
+        <Text
+          fontWeight="bold"
+          fontSize="120px"
+          lineHeight={1}
+          letterSpacing="-2px"
+          width="60%"
+        >
+          YOUR GO-TO SPOTIFY MUSIC GAME.
+        </Text>
+        <Text
+          bg="#4CABFF"
+          borderColor="black"
+          borderRadius="20px"
+          borderStyle="solid"
+          borderWidth={2}
+          fontFamily="Bebas Neue"
+          fontSize="20px"
+          justifySelf="start"
+          onClick={this.onClick}
+          p="8px"
+        >
+          I’M READY TO CUE IN
+        </Text>
+      </Box>
     );
   }
 }
