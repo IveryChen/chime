e4c7e592bbcde091f408fd234d97252fe75a00f2
IveryChen <ivy_chen1@brown.edu>
2024-11-22 00:02:29 -0800

    responsive improvement to Home

--- frontend/src/routes/Home/index.jsx
+++ frontend/src/routes/Home/index.jsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { Component } from "react";
 import { FaSpotify } from "react-icons/fa6";
 
+import cassette_temp from "../../assets/cassette_temp.png";
 import Box from "../../components/Box";
 import Text from "../../components/Text";
 
@@ -12,6 +13,8 @@ const StyledBar = styled(Box)`
 
   @media (min-width: 768px) {
     align-items: start;
+    display: grid;
+    grid-template-rows: 1fr auto;
     height: 100%;
     width: 56px;
   }
@@ -34,6 +37,16 @@ const StyledBox = styled(Box)`
   }
 `;
 
+const StyledHeading = styled(Text)`
+  font-size: 64px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    font-size: 120px;
+    width: 60%;
+  }
+`;
+
 const StyledText = styled(Text)`
   transform: rotate(0 deg);
 
@@ -42,12 +55,20 @@ const StyledText = styled(Text)`
   }
 `;
 
+const StyledTextChinese = styled(Text)`
+  transform: rotate(0 deg);
+
+  @media (min-width: 768px) {
+    transform: rotate(0 deg);
+  }
+`;
+
 export default class Home extends Component {
   onClick = () => (window.location.href = "/spotify-auth");
 
   render() {
     return (
-      <StyledBox display="grid">
+      <StyledBox display="grid" gap="32px">
         <StyledBar
           bg="#F7FFF9"
           borderColor="black"
@@ -61,18 +82,15 @@ export default class Home extends Component {
           <StyledText fontFamily="Oswald" fontSize="24px">
             CASSETTE
           </StyledText>
+          <StyledTextChinese fontFamily="Oswald" fontSize="24px">
+            盒式磁帶
+          </StyledTextChinese>
         </StyledBar>
-        <Box display="grid" gap="24px" height="100%">
-          <Text
-            fontWeight="bold"
-            fontSize="120px"
-            lineHeight={1}
-            textAlign="start"
-            letterSpacing="-2px"
-            width="60%"
-          >
+        <Box display="grid" gap="32px" height="100%">
+          <StyledHeading fontWeight="bold" letterSpacing="-2px" lineHeight={1}>
             YOUR GO-TO SPOTIFY MUSIC GAME.
-          </Text>
+          </StyledHeading>
+          <Box as="img" src={cassette_temp} />
           <Text
             alignSelf="start"
             bg="#4CABFF"
@@ -85,7 +103,7 @@ export default class Home extends Component {
             fontFamily="Bebas Neue"
             fontSize="20px"
             gap="8px"
-            justifySelf="start"
+            justifySelf="center"
             onClick={this.onClick}
             p="8px"
             userSelect="none"
