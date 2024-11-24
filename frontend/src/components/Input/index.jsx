242ecac157f5717cad2e2fda462ce27d3fb5a6c1
IveryChen <ivy_chen1@brown.edu>
2024-11-23 18:41:43 -0800

    add styling to Input

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,6 +1,32 @@
+import styled from "@emotion/styled";
 import React from "react";
 
 import Box from "../Box";
+import Text from "../Text";
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: ${(props) => props.padding || "16px"};
+  border-color: ${(props) => props.borderColor || "black"};
+  border-radius: ${(props) => props.borderRadius || "16px"};
+  border-style: ${(props) => props.borderStyle || "solid"};
+  border-width: ${(props) => props.borderWidth || "1px"};
+  background: ${(props) => props.background || "transparent"};
+  color: ${(props) => props.color || "white"};
+  font-size: ${(props) => props.fontSize || "16px"};
+  font-family: ${(props) => props.fontFamily || "inherit"};
+
+  &::placeholder {
+    color: ${(props) => props.placeholderColor || "#666"};
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${(props) => props.focusBorderColor || "#666"};
+  }
+
+  transition: all 0.2s ease-in-out;
+`;
 
 export default class Input extends React.PureComponent {
   handleChange = (e) => {
@@ -11,16 +37,48 @@ export default class Input extends React.PureComponent {
   };
 
   render() {
-    const { value, placeholder, required = true } = this.props;
+    const {
+      background,
+      borderColor,
+      borderRadius,
+      borderStyle,
+      borderWidth,
+      color,
+      focusBorderColor,
+      fontFamily,
+      fontSize,
+      label,
+      padding,
+      placeholder,
+      placeholderColor,
+      required = true,
+      value,
+    } = this.props;
 
     return (
-      <Box>
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={value}
+      <Box display="grid">
+        {label && (
+          <Text fontFamily="Bebas Neue" fontSize="24px" px="4px">
+            {label}
+          </Text>
+        )}
+        <StyledInput
+          background={background}
+          borderColor={borderColor}
+          borderRadius={borderRadius}
+          borderStyle={borderStyle}
+          borderWidth={borderWidth}
+          color={color}
+          focusBorderColor={focusBorderColor}
+          fontFamily={fontFamily}
+          fontSize={fontSize}
           onChange={this.handleChange}
+          padding={padding}
+          placeholder={placeholder}
+          placeholderColor={placeholderColor}
           required={required}
+          type="text"
+          value={value}
         />
       </Box>
     );
