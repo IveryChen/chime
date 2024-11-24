27255574a8cb26530140bb1aedff8851812ce63c
IveryChen <ivy_chen1@brown.edu>
2024-11-23 18:56:08 -0800

    remove extra box in IconButton

--- frontend/src/components/IconButton/index.jsx
+++ frontend/src/components/IconButton/index.jsx
@@ -7,25 +7,23 @@ export default class IconButton extends React.PureComponent {
   render() {
     const { Icon, label, ...restProps } = this.props;
     return (
-      <Box display="grid" gap="16px">
-        <Text
-          alignItems="center"
-          borderRadius="16px"
-          borderStyle="solid"
-          borderWidth={1}
-          cursor="pointer"
-          display="flex"
-          fontFamily="Bebas Neue"
-          fontSize="24px"
-          gap="6px"
-          lineHeight={1}
-          p="8px"
-          {...restProps}
-        >
-          {label}
-          {Icon && <Box as={Icon} size={20} />}
-        </Text>
-      </Box>
+      <Text
+        alignItems="center"
+        borderRadius="16px"
+        borderStyle="solid"
+        borderWidth={1}
+        cursor="pointer"
+        display="flex"
+        fontFamily="Bebas Neue"
+        fontSize="24px"
+        gap="6px"
+        lineHeight={1}
+        p="8px"
+        {...restProps}
+      >
+        {label}
+        {Icon && <Box as={Icon} size={20} />}
+      </Text>
     );
   }
 }
