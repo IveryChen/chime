f3f269f411eb35ff5a21736ed1383eedb313711d
IveryChen <ivy_chen1@brown.edu>
2024-11-23 21:56:18 -0800

    user withRouter in Game component

--- frontend/src/routes/Game/index.jsx
+++ frontend/src/routes/Game/index.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { branch } from "baobab-react/higher-order";
 
-export default class Game extends React.PureComponent {
+function withRouter(Component) {
+  return function ComponentWithRouter(props) {
+    const params = useParams();
+    return <Component {...props} params={params} />;
+  };
+}
+
+class Game extends React.PureComponent {
+  componentDidMount() {
+    // const { roomCode } = this.props.params;
+    // Connect to your multiplayer socket here using the roomCode
+    // Example: socket.emit('join-room', roomCode);
+  }
+
+  componentWillUnmount() {
+    // Cleanup socket connection when leaving
+    // Example: socket.emit('leave-room', roomCode);
+  }
+
   render() {
-    return null;
+    const { roomCode } = this.props.params;
+
+    return (
+      <div>
+        <h1>Game Room: {roomCode}</h1>
+        {/* Your game UI */}
+      </div>
+    );
   }
 }
+
+// Export with both router and Baobab state
+export default withRouter(
+  branch(
+    {
+      user: ["user"],
+      players: ["game", "players"],
+      gameState: ["game", "state"],
+    },
+    Game
+  )
+);
