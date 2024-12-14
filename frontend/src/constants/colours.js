0e324903d6db2668b840b7a0bb031148281fcf60
IveryChen <ivy_chen1@brown.edu>
2024-12-14 15:35:41 -0800

    add colour to colours constant

--- frontend/src/constants/colours.js
+++ frontend/src/constants/colours.js
@@ -1,3 +1,8 @@
-const theme = { blue: "#4CABFF", yellow: "#ffd602" };
+const theme = {
+  blue: "#4CABFF",
+  gray: "#E0E0E0",
+  green: "#70FF56",
+  yellow: "#ffd602",
+};
 
 export default theme;
