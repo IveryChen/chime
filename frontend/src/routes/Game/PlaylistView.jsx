70754c6374577f4fea358ee92fea8debd2e58e8b
IveryChen <ivy_chen1@brown.edu>
2024-12-14 15:41:05 -0800

    use socket connection in PlaylistView

--- frontend/src/routes/Game/PlaylistView.jsx
+++ frontend/src/routes/Game/PlaylistView.jsx
@@ -1,5 +1,6 @@
 import { includes, map } from "lodash";
 import React from "react";
+import { Async } from "react-async";
 
 import theme from "../../constants/colours";
 import Box from "../../components/Box";
@@ -9,40 +10,86 @@ import Logo from "../../components/Logo";
 import Players from "../../components/Players";
 import Playlist from "../../components/Playlist";
 import Text from "../../components/Text";
-
-import handleSubmitSelection from "./handleSubmitSelection";
+import socketService from "../../services/socket";
 
 export default class PlaylistView extends React.PureComponent {
   state = {
     selectedPlaylists: [],
+    status: null,
+    submittedPlayers: new Set(),
+  };
+
+  componentDidMount() {
+    console.log("PlaylistView mounting");
+    const boundHandler = this.handleAllPlaylistsSubmitted.bind(this);
+    socketService.on("all_playlists_submitted", boundHandler);
+    socketService.on("playlist_submitted", this.handlePlaylistSubmitted);
+
+    // Debug existing handlers
+    console.log("Current handlers:", socketService.eventHandlers);
+  }
+
+  componentWillUnmount() {
+    socketService.off("playlist_submitted", this.handlePlaylistSubmitted);
+    socketService.off(
+      "all_playlists_submitted",
+      this.handleAllPlaylistsSubmitted
+    );
+  }
+
+  handlePlaylistSubmitted = ({ player_id, submitted }) => {
+    this.setState((prev) => {
+      const newSubmittedPlayers = new Set(prev.submittedPlayers);
+      if (submitted) {
+        newSubmittedPlayers.add(player_id);
+      } else {
+        newSubmittedPlayers.delete(player_id);
+      }
+      return {
+        submittedPlayers: newSubmittedPlayers,
+        status: "waiting",
+      };
+    });
+  };
+
+  handleAllPlaylistsSubmitted = ({ status }) => {
+    console.log("Handler called with status:", status);
+    this.setState({ status }, () => {
+      if (status === "success") {
+        this.props.onChangeGameStage("game");
+      }
+    });
   };
 
   onChangeSelectedPlaylists = (playlistId) => {
-    this.setState((prevState) => ({
-      selectedPlaylists: includes(prevState.selectedPlaylists, playlistId)
-        ? prevState.selectedPlaylists.filter((id) => id !== playlistId)
-        : [...prevState.selectedPlaylists, playlistId],
+    this.setState((prev) => ({
+      selectedPlaylists: includes(prev.selectedPlaylists, playlistId)
+        ? prev.selectedPlaylists.filter((id) => id !== playlistId)
+        : [...prev.selectedPlaylists, playlistId],
     }));
   };
 
-  handleSubmit = () => {
+  submitPlaylists = async () => {
     const { playerId, roomCode } = this.props;
-    const { selectedPlaylists } = this.state;
+    const { selectedPlaylists, submittedPlayers } = this.state;
 
-    const status = handleSubmitSelection({
-      playerId,
+    socketService.emit("select_playlists", {
       roomCode,
-      selectedPlaylists,
+      playerId,
+      playlistIds: selectedPlaylists,
     });
 
-    console.log(status);
+    this.setState({
+      status: "waiting",
+      submittedPlayers: new Set([...submittedPlayers, playerId]),
+    });
 
-    this.props.onChangeGameStage("game");
+    return Promise.resolve({ status: "waiting" });
   };
 
   render() {
-    const { players, playlists } = this.props;
-    const { selectedPlaylists } = this.state;
+    const { players } = this.props;
+    const { submittedPlayers } = this.state;
 
     return (
       <>
@@ -55,7 +102,7 @@ export default class PlaylistView extends React.PureComponent {
           gridTemplateRows="auto 1fr auto"
           overflow="hidden"
         >
-          <Players data={players} />
+          <Players data={players} submittedPlayers={submittedPlayers} />
           <Text
             fontSize={24}
             fontWeight="bold"
@@ -65,36 +112,58 @@ export default class PlaylistView extends React.PureComponent {
           >
             SELECT PLAYLISTS
           </Text>
-          <Box
-            display="grid"
-            gap="16px"
-            gridTemplateColumns="1fr 1fr"
-            overflow="auto"
-          >
-            {map(playlists, (data, index) => {
-              if (!data) {
-                return;
-              }
-
-              return (
-                <Playlist
-                  data={data}
-                  key={index}
-                  onChangeSelectedPlaylists={this.onChangeSelectedPlaylists}
-                  selectedPlaylists={selectedPlaylists}
-                />
-              );
-            })}
-          </Box>
-          <IconButton
-            bg={theme.blue}
-            fontSize={16}
-            justifySelf="end"
-            label="DONE"
-            onClick={this.handleSubmit}
-          />
+          <Async deferFn={this.submitPlaylists}>{this.renderBody}</Async>
         </Box>
       </>
     );
   }
+
+  renderBody = ({ isPending, run }) => {
+    const { playlists, players, playerId } = this.props;
+    const { selectedPlaylists } = this.state;
+    const { submittedPlayers, status } = this.state;
+    const hasSubmitted = submittedPlayers.has(playerId);
+
+    return (
+      <>
+        {status === "waiting" && (
+          <Text bg={theme.yellow}>
+            Waiting for other players... ({submittedPlayers.size} /{" "}
+            {players.length} ready)
+          </Text>
+        )}
+        <Box
+          display="grid"
+          gap="16px"
+          gridTemplateColumns="1fr 1fr"
+          overflow="auto"
+          opacity={hasSubmitted ? 0.5 : 1}
+          pointerEvents={hasSubmitted ? "none" : "auto"}
+        >
+          {map(playlists, (data, index) => {
+            if (!data) {
+              return;
+            }
+
+            return (
+              <Playlist
+                data={data}
+                key={index}
+                onChangeSelectedPlaylists={this.onChangeSelectedPlaylists}
+                selectedPlaylists={selectedPlaylists}
+              />
+            );
+          })}
+        </Box>
+        <IconButton
+          bg={theme.blue}
+          disabled={isPending || hasSubmitted}
+          fontSize={16}
+          justifySelf="end"
+          label="DONE"
+          onClick={run}
+        />
+      </>
+    );
+  };
 }
