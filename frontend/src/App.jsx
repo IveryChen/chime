2d5a07784657470b032c7014165d8f5066a8ac7e
IveryChen <ivy_chen1@brown.edu>
2024-12-16 21:57:56 -0800

    wrap App with fontLoader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { root } from "baobab-react/higher-order";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import FontLoader from "./components/FontLoader";
 import SpotifyAuth from "./components/SpotifyAuth";
 
 import Home from "./routes/Home";
@@ -14,14 +15,16 @@ import "./App.css";
 class App extends React.PureComponent {
   render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/lobby" element={<Lobby />} />
-          <Route path="/spotify-auth" element={<SpotifyAuth />} />
-          <Route path="/game/:roomCode" element={<Game />} />
-        </Routes>
-      </BrowserRouter>
+      <FontLoader>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lobby" element={<Lobby />} />
+            <Route path="/spotify-auth" element={<SpotifyAuth />} />
+            <Route path="/game/:roomCode" element={<Game />} />
+          </Routes>
+        </BrowserRouter>
+      </FontLoader>
     );
   }
 }
