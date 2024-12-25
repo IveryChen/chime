0007d5aaf4485103fcf0fd838591cde9e1df899f
IveryChen <ivy_chen1@brown.edu>
2024-12-25 12:26:57 -0800

    move s3 links to constants file

---
+++ frontend/src/routes/Home/constants.js
@@ -0,0 +1,5 @@
+export const HDRI =
+  "https://cassetteapp.s3.us-east-2.amazonaws.com/studio_small_05_4k.hdr";
+
+export const modelURL =
+  "https://cassetteapp.s3.us-east-2.amazonaws.com/Vintage+Cassette/CassetteClean.fbx";
