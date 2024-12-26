cfc61b5d07dc2286e9cad84f9ff58899e066fc27
IveryChen <ivy_chen1@brown.edu>
2024-12-26 08:54:15 -0800

    detect emulators in getPlatform

--- frontend/src/utils/getPlatform.js
+++ frontend/src/utils/getPlatform.js
@@ -1,19 +1,24 @@
 export default function getPlatform() {
   const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  // iOS detection
+  const isEmulator =
+    /Chrome/.test(navigator.userAgent) && /Mobile/.test(navigator.userAgent);
+
+  const isMobile =
+    /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+      userAgent
+    );
+
+  if (isEmulator || !isMobile) {
+    return "Desktop";
+  }
+
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     return "iOS";
   }
 
-  // Android detection
   if (/android/i.test(userAgent)) {
     return "Android";
-  }
-
-  // Desktop detection
-  if (window.innerWidth >= 768) {
-    return "Desktop";
   }
 
   return "Mobile";
