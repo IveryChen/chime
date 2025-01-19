5432d27a045ade66a8a8c646a40f366e51a2a2f0
IveryChen <ivy_chen1@brown.edu>
2025-01-18 22:34:40 -0800

    render player themselve first in Players

--- frontend/src/components/Players/index.jsx
+++ frontend/src/components/Players/index.jsx
@@ -1,5 +1,5 @@
 import { branch } from "baobab-react/higher-order";
-import { map } from "lodash";
+import { map, sortBy } from "lodash";
 import React from "react";
 
 import Box from "../Box";
@@ -7,9 +7,20 @@ import Text from "../Text";
 
 import Player from "./Player";
 
+function sortPlayers(players, currentUserId) {
+  if (!players || !currentUserId) return players;
+
+  return sortBy(players, (player) => (player.id === currentUserId ? 0 : 1));
+}
+
 class Players extends React.PureComponent {
   render() {
-    const { currentRoom, gameState, submittedPlayers = new Set() } = this.props;
+    const {
+      currentRoom,
+      gameState,
+      submittedPlayers = new Set(),
+      user,
+    } = this.props;
     const { status: gameStatus } = currentRoom;
     const isPlaying = gameStatus === "playing";
 
@@ -20,6 +31,8 @@ class Players extends React.PureComponent {
     const { currentPlayer, scores = {} } = gameState;
     const { players } = currentRoom;
 
+    const sortedPlayers = sortPlayers(players, user?.player?.id);
+
     return (
       <Box
         alignSelf="start"
@@ -28,7 +41,7 @@ class Players extends React.PureComponent {
         gridTemplateColumns="repeat(auto-fill, minmax(1fr))"
         justifyContent="center"
       >
-        {map(players, (player) => {
+        {map(sortedPlayers, (player) => {
           const { id } = player;
           const score = scores[id] || 0;
 
@@ -54,6 +67,10 @@ class Players extends React.PureComponent {
 }
 
 export default branch(
-  { currentRoom: ["games", "currentRoom"], gameState: ["games", "gameState"] },
+  {
+    currentRoom: ["games", "currentRoom"],
+    gameState: ["games", "gameState"],
+    user: ["user"],
+  },
   Players
 );
